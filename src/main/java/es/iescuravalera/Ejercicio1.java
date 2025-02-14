e1d8f528b70df425793c2cfb1e443694edb2bd17
Laura Guillén Cárceles <laura.sistemas.aplicaciones@gmail.com>
2025-02-14 22:16:57 +0100

    Cambios en comprar de Ejercicio.java

--- src/main/java/es/iescuravalera/Ejercicio1.java
+++ src/main/java/es/iescuravalera/Ejercicio1.java
@@ -1,10 +1,10 @@
 package es.iescuravalera;
 
 public class Ejercicio1 {
-    public int comparar (int a, int b) {
-        if (a>b)
+    public int comparar (int a1, int a2) {
+        if (a1>a2)
             return 1;
-        else if (a<b)
+        else if (a1<a2)
             return -1;
         return 0;
     }
