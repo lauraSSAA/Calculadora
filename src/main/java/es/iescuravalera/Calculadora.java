62bbf036a30583052a1fbc8b849e8cdf0a6ce7ec
Laura Guillén Cárceles <laura.sistemas.aplicaciones@gmail.com>
2025-02-14 22:14:27 +0100

    Cambios en Calculadora.java, en argumentos del constructor

--- src/main/java/es/iescuravalera/Calculadora.java
+++ src/main/java/es/iescuravalera/Calculadora.java
@@ -4,7 +4,7 @@ public class Calculadora {
     private int num1;
     private int num2;
 
-    public Calculadora(int a, int b) {
+    public Calculadora(int num1, int num2) {
         num1 = a;
         num2 = b;
     }
